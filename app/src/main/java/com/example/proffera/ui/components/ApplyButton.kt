9cf5555382e5d5fded82ff3f67f23c168e2ee46c
Rayhan Illyas <rayhan205732@gmail.com>
2023-06-09 05:11:31 +0700

    create gabung proyek component button

---
+++ app/src/main/java/com/example/proffera/ui/components/ApplyButton.kt
@@ -0,0 +1,66 @@
+package com.example.proffera.ui.components
+
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proffera.ui.theme.DarkOrange
+import com.example.proffera.ui.theme.ProfferaTheme
+import com.example.proffera.ui.theme.White
+import com.example.proffera.ui.theme.WhiteSmoke
+
+@Composable
+fun ApplyButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(containerColor = DarkOrange),
+        modifier = modifier
+            .width(220.dp)
+            .height(52.dp)
+            .shadow(
+                2.dp,
+                shape = RoundedCornerShape(25.dp),
+                ambientColor = DarkOrange.copy(0.25f),
+                spotColor = DarkOrange.copy(0.25f)
+            )
+
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleLarge,
+            color = White,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ApplyButtonPreview() {
+    ProfferaTheme() {
+        Surface(color = WhiteSmoke) {
+            ApplyButton(
+                text = "Gabung Proyek",
+                onClick = {},
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
