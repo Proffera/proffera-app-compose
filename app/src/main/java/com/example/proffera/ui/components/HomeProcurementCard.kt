fd7023c6078911da7e8154211637b4136be5dfe1
Rayhan Illyas <rayhan205732@gmail.com>
2023-06-09 01:21:03 +0700

    finishing homecard v2

--- app/src/main/java/com/example/proffera/ui/components/HomeProcurementCard.kt
+++ app/src/main/java/com/example/proffera/ui/components/HomeProcurementCard.kt
@@ -1,10 +1,12 @@
 package com.example.proffera.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.BookmarkBorder
 import androidx.compose.material.icons.filled.HourglassEmpty
 import androidx.compose.material3.*
@@ -13,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -21,6 +22,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.proffera.R
 import com.example.proffera.data.remote.model.Procurement
 import com.example.proffera.ui.theme.*
@@ -55,6 +57,7 @@ fun HomeProcurement(
                         contentDescription = "Project Picture",
                         modifier = Modifier
                             .size(size = 50.dp)
+                            .border(1.dp, DarkOrange, CircleShape)
                             .clip(CircleShape)
                     )
                     Spacer(
@@ -64,8 +67,7 @@ fun HomeProcurement(
                     Column {
                         Text(
                             text = procurement.projectName,
-                            style = MaterialTheme.typography.titleMedium,
-                            color = Dark,
+                            style = MaterialTheme.typography.titleLarge,
                             textAlign = TextAlign.Start,
                             maxLines = 2,
                             overflow = TextOverflow.Ellipsis
@@ -87,6 +89,83 @@ fun HomeProcurement(
                         .wrapContentWidth(Alignment.End)
                 )
             }
+//            unknown and price
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(top = 12.dp)
+            ) {
+                Box(Modifier.weight(0.5f)) {
+                    Text(
+                        text = "Deskripsi",
+                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                    )
+                }
+                Text(
+                    text = procurement.costEstimate,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
+//            Description
+            Text(
+                text = procurement.description,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier
+                    .width(width = 300.dp)
+            )
+//            footer card
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Box(Modifier.weight(1f)) {
+                    Card(
+                        modifier = Modifier
+                            .width(100.dp)
+                            .height(height = 40.dp)
+                            .padding(top = 8.dp),
+                        shape = RoundedCornerShape(30.dp),
+                        colors = CardDefaults.cardColors(containerColor = Blue),
+                    ) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = procurement.status,
+                                style = MaterialTheme.typography.bodyMedium.copy(
+                                    fontWeight = FontWeight.ExtraBold,
+                                    fontSize = 12.sp
+                                ),
+                                color = Color.White,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                        }
+                    }
+                }
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 8.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.AccessTime,
+                        contentDescription = "deadline",
+                        modifier = Modifier
+                            .height(24.dp)
+                    )
+                    Text(
+                        text = procurement.deadlineEstimate,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(start = 8.dp, bottom = 2.dp)
+                    )
+                }
+            }
         }
     }
 }
@@ -102,7 +181,7 @@ fun HomeProcurementPreview() {
                     instansiName = "Kemristekdikti",
                     projectName = "Pembangunan Technopark",
                     city = "Jakarta",
-                    costEstimate = "Rp.500 Juta",
+                    costEstimate = "Rp.500jt",
                     description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. t enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     status = "Dalam Proses",
                     deadlineEstimate = "6 Bulan",
--- app/src/main/java/com/example/proffera/ui/theme/Type.kt
+++ app/src/main/java/com/example/proffera/ui/theme/Type.kt
@@ -13,50 +13,57 @@ val Typography = Typography(
     headlineMedium = TextStyle(
         fontFamily = FontFamily(Font(R.font.n_sans_bold)),
         fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-        lineHeight = 33.sp,
-        letterSpacing = 0.sp
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp,
+        color = Dark
     ),
     headlineSmall = TextStyle(
         fontFamily = FontFamily(Font(R.font.n_sans_bold)),
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
+        color = Dark
     ),
     titleLarge = TextStyle(
         fontFamily = FontFamily(Font(R.font.n_sans_bold)),
         fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-        lineHeight = 33.sp,
-        letterSpacing = 0.sp
-    ),
-    titleMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.n_sans_bold)),
-        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 22.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
+        color = Dark
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.n_sans_sb)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp,
+        color = Dark
     ),
     titleSmall = TextStyle(
         fontFamily = FontFamily(Font(R.font.n_sans_medium)),
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
+        color = Dark
     ),
     bodyMedium = TextStyle(
         fontFamily = FontFamily(Font(R.font.n_sans_regular)),
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp,
+        color = Dark
     ),
     bodySmall = TextStyle(
         fontFamily = FontFamily(Font(R.font.n_sans_light)),
         fontWeight = FontWeight.Light,
         fontSize = 14.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
+        color = Dark
     ),
 )
